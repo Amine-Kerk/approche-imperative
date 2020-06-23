1278f426a9510877f21ef0d611b9495418a5b427
Amine KERKOUB <kerk.amine@gmail.com>
2020-06-23 12:42:32 +0200

    tp variable

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte a=5;
+		short b=10;
+		int c=20;
+		long d=20000L;
+		float e=3.2F;
+		double f=1.8D;
+		char  g='b';
+		boolean h=false;
+		String i="bonjour";
+		
+
+	}
+	system.out.println(c);
+
+}
